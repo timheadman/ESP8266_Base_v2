76b03d70c475ae865ad7eb2591eab41dc2eca0aa
Tim <timhdm@ya.ru>
2024-02-14 15:10:52 +0300

    add time and timers

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,34 @@
 #include "main.h"
 // AP IP address: 192.168.4.1, set manual IP in network settings
-
-void configModeCallback(WiFiManager *myWiFiManager) {
-  Serial.println("Entered config mode");
-  Serial.println(WiFi.softAPIP());
-  // if you used auto generated SSID, print it
-  Serial.println(myWiFiManager->getConfigPortalSSID());
-}
+GTimer timerOneSecond(MS);
+struct tm timeStructureNow;
 
 void setup() {
   Serial.begin(115200);
 
   WiFiManager wifiManager;
   wifiManager.setTimeout(60);
-  // set callback that gets called when connecting to previous WiFi fails, and
-  // enters Access Point mode
-  wifiManager.setAPCallback(configModeCallback);
   wifiManager.autoConnect();
-  
+
+  configTime("MSK-3", 0, "pool.ntp.org");
+  timerOneSecond.setInterval(1000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (timerOneSecond.isReady()) triggerOneSecond();
+}
+
+void triggerOneSecond() {
+  updateTime();
+  if (time(nullptr) > 1700000000)
+    Serial.printf("%lld (%d:%d:%d)\n", time(nullptr), timeStructureNow.tm_hour,
+                  timeStructureNow.tm_min, timeStructureNow.tm_sec);
+  else
+    Serial.printf("NTR synchronization failed\n");
+}
+
+void updateTime() {
+  time_t unixTimeNow;
+  time(&unixTimeNow);
+  localtime_r(&unixTimeNow, &timeStructureNow);
 }
--- src/main.h
+++ src/main.h
@@ -4,6 +4,10 @@
 #include <DNSServer.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266WiFi.h>
+#include <GyverTimer.h>
 #include <WiFiManager.h>
+
+void updateTime();
+void triggerOneSecond();
 
 #endif  // ESP8266_WIFI_BASE_H_
