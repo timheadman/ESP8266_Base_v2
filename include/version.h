3ac761c9970a9162357ef8ebfa21ae9665cc2bba
Tim <timhdm@ya.ru>
2024-03-21 13:02:57 +0300

    minor

--- include/version.h
+++ include/version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "0.0.347"
+        #define VERSION "0.0.349"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2024-03-21 13:00"
+        #define BUILD_TIMESTAMP "2024-03-21 13:02"
     #endif
     
